0a0853763dca68e1cd6565bfc3d85cab985fb707
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 21:36:23 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -7,10 +7,10 @@ class Dancer
   
   include FancyDance::InstanceMethods 
   
-  # attr_accessor :name 
+  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name
-  # end 
+  def initialize(name)
+    @name = name
+  end 
 
 end 
